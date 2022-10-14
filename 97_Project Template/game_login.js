2835175ebd34d10c9bb5bc2e5fa66dc129f8c227
aadyashah <104903295+aadyashah@users.noreply.github.com>
2022-10-14 11:21:59 +0530

    Add files via upload

---
+++ 97_Project Template/game_login.js
@@ -0,0 +1,11 @@
+// Create function addUser()
+function aadUser() {
+
+}
+  // Get value of user by id player1_name_input and player2_name_input
+
+  // Store these values locally
+
+  //Assign "game_page.html" to window.location
+
+
